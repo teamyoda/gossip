0654294041d8bf09f05dcf551cc431d1fad269b0
Ben Anglin <ben.anglin@gmail.com>
2012-11-03 16:51:37 -0400

    Adding message for adding a new neighbor

--- include/message.hrl
+++ include/message.hrl
@@ -5,3 +5,6 @@
 -record(message, {from, %% PID of the sender
                   function, %% atom representing the function to send to 
                   data}). %% the secret being passed
+
+%% Message to manage neighbors
+-record(add_neighbor, {neighbor}). %% PID of the new neighbor
