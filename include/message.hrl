79cd92b4e5e6cb42bfddd48babd5a417032a7a11
Ben Anglin <ben.anglin@gmail.com>
2012-11-03 16:26:40 -0400

    Adding a messages header for explicit message interface

---
+++ include/message.hrl
@@ -0,0 +1,7 @@
+%% This header should contain all message formats used in inter-node 
+%% communication
+
+%% Basic message passing format
+-record(message, {from, %% PID of the sender
+                  function, %% atom representing the function to send to 
+                  data}). %% the secret being passed
