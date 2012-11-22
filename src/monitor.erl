b3f9a955fa5d143fd0be6f35dbff7d9f6b8f46c1
Ben Anglin <ben.anglin@gmail.com>
2012-11-21 20:09:09 -0500

    Added the ability to make neighbor node-pairs and to step a given list of nodes

--- src/monitor.erl
+++ src/monitor.erl
@@ -1,5 +1,5 @@
 -module(monitor).
--export([start/0, execute/0, get_min/1, get_max/1, get_average/1]).
+-export([start/0, execute/0, get_min/1, get_max/1, get_average/1, make_neighbors/2, step/1]).
 -include("include/message.hrl").
 
 %% Monitor for checking the status of any gossip node
@@ -12,15 +12,30 @@ start() ->
 
 %% Ask any node what its min is
 get_min(To_PID) ->
-    To_PID ! #request{from=monitor, field=min}.
+    To_PID ! #request{from=monitor, field=min},
+    ok.
 
 %% Ask any node what its max is
 get_max(To_PID) ->
-    To_PID ! #request{from=monitor, field=max}.
+    To_PID ! #request{from=monitor, field=max},
+    ok.
 
 %% Ask any node what its average is
 get_average(To_PID) ->
-    To_PID ! #request{from=monitor, field=average}.
+    To_PID ! #request{from=monitor, field=average},
+    ok.
+
+make_neighbors(PID_1, PID_2) ->
+    PID_1 ! #add_neighbor{neighbor=PID_2},
+    PID_2 ! #add_neighbor{neighbor=PID_1},
+    ok.
+
+step([]) ->
+    ok;
+
+step([Head | Rest]) ->
+    Head ! step,
+    step(Rest).
 
 execute() ->
     receive
