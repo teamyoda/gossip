70f74f6acc1b2f70df63d8945b9e9ee42007a370
Ben Anglin <ben.anglin@gmail.com>
2012-11-28 23:26:40 -0500

    Adding capability to store and retrieve fragments from any node

--- include/message.hrl
+++ include/message.hrl
@@ -9,13 +9,14 @@
 %% Message to manage neighbors
 -record(add_neighbor, {neighbor}). %% PID of the new neighbor
 
--record(fragment, {owner, data}).
+-record(fragment, {owner, sender, replyto, data, method}).
 
 -record(state, {min=0, %% Node's current minimum secret
                 max=0, %% Node's current maximum secret
                 average=0, % Node's current average secret
                 median=0, % Node's current average secret
-                fragment=[]}). %% Node's current fragment
+                fragment=[], %% Node's current fragment
+                fragment_list=[]}). %% List of fragments held by this node
 
 -record(request, {from, field}).
 
--- src/node.erl
+++ src/node.erl
@@ -1,14 +1,25 @@
 -module(node).
--export([execute/2, start_node/3, start_remote_node/3, start_node/1, send_min/2, send_max/2, send_average/2, median/1, forward_fragment/2]).
+-export([execute/2, 
+        start_node/3, 
+        start_remote_node/3, 
+        start_node/1, 
+        send_min/2, 
+        send_max/2, 
+        send_average/2, 
+        median/1, 
+        send_fragment/2, 
+        store_fragment/2, 
+        process_fragment/2]).
+
 -include("include/message.hrl").
 
 %% Public API
 
 %% Start a new node with a fragment
 start_node(Fragment) when is_list(Fragment) ->
-    State = process_fragment(Fragment),
-    io:format("starting node with ~p~n", [State]),
-    spawn(node, execute, [[], State]).
+    Node = spawn(node, execute, [[], #state{}]),
+    Node ! #fragment{owner=Node, data=Fragment, method=store},
+    Node.
 
 %% Start a new node with a min, max, and average
 start_node(Min, Max, Average) ->
@@ -26,6 +37,7 @@ execute(Neighbors, State) ->
     Max = State#state.max,
     Median = State#state.median,
     Fragment = State#state.fragment,
+    FragmentList = State#state.fragment_list,
 
     receive
         %% Get a neighbor's min and process it
@@ -48,18 +60,57 @@ execute(Neighbors, State) ->
             New_Median = median([Median, Data]),
             execute(Neighbors, State#state{median=New_Median});
 
-        %% Update a fragment or pass it on
-        #message{function=fragment, from=From, data=Data} ->
-            New_State = 
-                if Data#fragment.owner == self() ->
-                    %% Update node state using the new fragment
-                    process_fragment(Fragment);                  
+        %% If we have a copy of the fragment, send it back. Otherwise, forward
+        %% the request to all neighbors except the sender.
+        Frag = #fragment{owner=Owner, sender=Sender, replyto=FinalDest, method=request} ->
+            Entry = lists:keyfind(Owner, #fragment.owner, FragmentList),
+            io:format("Entry result on request is: ~p~n", [Entry]),
+            if 
+                Entry /= false ->
+                    io:format("Attempting to send fragment response~n"),
+                    send_fragment([Sender], Entry#fragment{replyto=FinalDest, method=reply});
                 true ->
-                    %% Forward the fragment to all neighbors except the sender
-                    forward_fragment(Neighbors -- [From], Fragment),
+                    io:format("Forwarding response to neighbors~n"),
+                    send_fragment(Neighbors -- [Sender], Frag)
+            end,
+            execute(Neighbors, State);
+
+        %% If we get a fragment to store, check if it belongs to us and
+        %% process it if so. If we hold a copy of it, update our copy and
+        %% forward to our neighbors. If it does not belong to us and we
+        %% don't hold a copy of it, just forward the fragment to our neighbors.
+        Frag = #fragment{owner=Owner, sender=Sender, method=store} ->
+            Entry = lists:keyfind(Owner, #fragment.owner, FragmentList),
+            NewState = 
+            if 
+                Owner == self() ->
+                    %% We own this fragment
+                    TempState = process_fragment(Frag, State),
+                    store_fragment(Frag, TempState);
+                Entry /= false ->
+                    %% We store a copy of this fragment
+                    send_fragment(Neighbors -- [Sender], Frag),
+                    store_fragment(Frag, State);
+                true ->
+                    %% We just need to pass the fragment on
+                    send_fragment(Neighbors -- [Sender], Frag),
                     State
-                end,
-            execute(Neighbors, New_State);
+            end,
+            execute(Neighbors, NewState);
+
+        %% If we get a fragment with the method set to reply, then it is in
+        %% reply to a request we issued. Store it locally.
+        Frag = #fragment{sender=Sender, replyto=FinalDest, method=reply} ->
+            io:format("Node ~p got reply ~p", [self(), Frag]),
+            NewState = 
+            if
+                FinalDest == self() ->
+                    store_fragment(Frag, State);
+                true ->
+                    send_fragment(Neighbors -- [Sender], Frag),
+                    State
+            end,
+            execute(Neighbors, NewState);
 
         %% Get a new neighbor's PID and add it to our list
         #add_neighbor{neighbor=Neighbor} ->
@@ -87,16 +138,16 @@ execute(Neighbors, State) ->
             send_median(From, State),
             execute(Neighbors, State);
 
-
-        %% Get a request for our fragment and send it to the requestor
-        #request{from=From, field=fragment} ->
-            send_fragment(From, State),
+        #request{from=From, field=fragments} ->
+            From ! FragmentList,
             execute(Neighbors, State);
 
         %% Step the node
         step ->
             push(Neighbors, State),
             pull(Neighbors),
+            Frag = State#state.fragment,
+            send_fragment(Neighbors, Frag#fragment{owner=self(), sender=self(), method=store}),
             execute(Neighbors, State);
 
         %% Shut down the node
@@ -110,32 +161,43 @@ execute(Neighbors, State) ->
     end.
 
 %% Process a fragment and return a new state
-process_fragment(Fragment) ->
-    Average = average(Fragment),
-    Min = lists:min(Fragment),
-    Max = lists:max(Fragment),
-    Median = median(Fragment),
-    #state{min=Min, max=Max, average=Average, median=Median, fragment=Fragment}.
+process_fragment(Fragment, State) when is_record(Fragment, fragment) and is_record(State, state) ->
+    Data = Fragment#fragment.data,
+
+    Average = average(Data),
+    Min = lists:min(Data),
+    Max = lists:max(Data),
+    Median = median(Data),
+
+    State#state{min=Min, max=Max, average=Average, median=Median, fragment=Fragment}.
+
+store_fragment(Fragment, State) when is_record(Fragment, fragment) and is_record(State, state) ->
+    FragmentList = lists:keystore(Fragment#fragment.owner, 
+                                  #fragment.owner, 
+                                  State#state.fragment_list, 
+                                  Fragment),
+
+    State#state{fragment_list=FragmentList}.
+
+send_fragment([], _) -> ok;
+send_fragment([To | PIDS], Fragment) ->
+    To ! Fragment#fragment{sender=self()},
+    send_fragment(PIDS, Fragment).
 
 %% Find the median of a list
 median(List) ->
     Sorted = lists:sort(List),
-    if length(Sorted) rem 2 == 1 -> 
-        lists:nth((length(Sorted) div 2) + 1, Sorted);
-    true ->
-        First = lists:nth((length(Sorted) div 2), Sorted),
-        Second = lists:nth((length(Sorted) div 2) + 1, Sorted),
-        (First + Second)/2
+    if 
+        length(Sorted) rem 2 == 1 -> 
+            lists:nth((length(Sorted) div 2) + 1, Sorted);
+        true ->
+            First = lists:nth((length(Sorted) div 2), Sorted),
+            Second = lists:nth((length(Sorted) div 2) + 1, Sorted),
+            (First + Second)/2
     end.
 
 %% Find the average of a list
 average(List) -> lists:sum(List) / length(List).
-
-forward_fragment([], _) -> ok;
-
-forward_fragment([To | Neighbors], Fragment) ->
-    To ! #message{function=fragment, from=self(), data=Fragment},
-    forward_fragment(Neighbors, Fragment).
 
 push([], _) ->
     ok;
@@ -163,10 +225,6 @@ send_average(PID, State) ->
 
 send_median(PID, State) ->
     PID ! #message{function=median, from=self(), data=State#state.median},
-    ok.
-
-send_fragment(PID, State) ->
-    PID ! #message{function=fragment, from=self(), data=State#state.fragment},
     ok.
 
 pull([]) ->
