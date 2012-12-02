0057a0f98b8feac7b839c431d2bd96c63f52a2f8
sbobovyc <stan.bobovych@gmail.com>
2012-12-01 21:13:55 -0500

    * Exp1 prints a header * Exp1 calculate % error for each node

--- src/exp1.erl
+++ src/exp1.erl
@@ -12,44 +12,54 @@ ceiling(X) ->
         false -> T + 1
 end.
 
+calc_error(Value, EstValue) ->
+    io:format("~p ~p \n", [Value, EstValue]),
+    (Value - EstValue) / Value.
+
 run(NumNodes, NumRounds, FileName) ->
     io:format("Starting monitor~n"),
     {Network, File} = monitor:create_network(NumNodes, fragments),
     io:format("Created network of ~p nodes~n", [length(Network)]),
     io:format("Will now run at most 1000 rounds~n"),
     {ok, FileDescriptor} = file:open(FileName, [write]), 
-    looper(FileDescriptor, Network, 0, NumRounds),
     FileInfo = monitor:get_file_stats(File),
-    file:close(FileDescriptor),
-    io:format("Actual values are:~n~p~n", [FileInfo]).
+    io:format("Actual values are:~n~p~n", [FileInfo]),
+    io:format("Rounds, Node, Min, Average, Median, Max, MinError, AvgError, MedError, MaxError\n"),
+    looper(FileDescriptor, Network, FileInfo, 0, NumRounds),
+    file:close(FileDescriptor).
 
-dump_nodes(Network, FileDescriptor, Rounds) ->
+dump_nodes(FileDescriptor, Network, FileInfo, Rounds) ->
     lists:foreach(
             fun(Node) ->
                 Results = monitor:query_node(Node),
-                dump_to_file(FileDescriptor, Rounds, Node, Results) end,
+                dump_to_file(FileDescriptor, Rounds, FileInfo, Node, Results) end,
                 Network
             ).
 
-dump_to_file(FileDescriptor, Rounds, Node, Results) ->
+dump_to_file(FileDescriptor, Rounds, FileInfo, Node, Results) ->
     [Min|Rest] = Results,
     [Average|Rest2] = Rest,
     [Fragments|Rest3] = Rest2,
     [Median|Rest4] = Rest3,
     [Max|Rest5] = Rest4,
-    io:format(FileDescriptor, "~p, ~p, ~p, ~p, ~p, ~p\n", [Rounds, Node, element(2, Min), element(2, Average), element(2, Median), element(2, Max)]),
+    MinError = calc_error(element(2,Min), element(2, lists:nth(1, FileInfo))),
+    MaxError = calc_error(element(2,Max), element(2, lists:nth(2, FileInfo))),
+    AvgError = calc_error(element(2,Average), element(2, lists:nth(3, FileInfo))),
+    MedianError = calc_error(element(2, Median), element(2, lists:nth(4, FileInfo))),
+    io:format(FileDescriptor, "~p, ~p, ~p, ~p, ~p, ~p , ~p, ~p, ~p, ~p\n", [Rounds, Node, element(2, Min), element(2, Average), element(2, Median), element(2, Max),
+                                                                                            MinError, AvgError, MedianError, MaxError]),
     ok.
 
-looper(_, _, Rounds, MaxRounds) when Rounds > MaxRounds -> ok;
-looper(FileDescriptor, Network, Rounds, MaxRounds) ->
+looper(_, _, _, Rounds, MaxRounds) when Rounds > MaxRounds -> ok;
+looper(FileDescriptor, Network, FileInfo, Rounds, MaxRounds) ->
     monitor:step(Network),
     case Rounds rem 10 == 0 of
         true ->
-            dump_nodes(Network, FileDescriptor, Rounds);
+            dump_nodes(FileDescriptor, Network, FileInfo, Rounds);
         false-> false
     end,
     timer:sleep(1),
-    looper(FileDescriptor, Network, Rounds + 1, MaxRounds).
+    looper(FileDescriptor, Network, FileInfo, Rounds + 1, MaxRounds).
 
 run_experiment() ->
     run(10000, 200, "10k200.txt").
